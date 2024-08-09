771d6d9486a5ab8a75106540f6bd60d7a58c70ba
kquewvin <117505337+kquewvin@users.noreply.github.com>
2024-08-09 15:08:38 -0400

    removed img icon

--- script.js
+++ script.js
@@ -1,46 +1,48 @@
+/*SEARCH BY USING A CITY NAME (e.g. athens) OR A COMMA-SEPARATED CITY NAME ALONG WITH THE COUNTRY CODE (e.g. athens,gr)*/
 const form = document.querySelector(".top-banner form");
+const input = document.querySelector(".top-banner input");
+const msg = document.querySelector(".top-banner .msg");
+const list = document.querySelector(".ajax-section .cities");
+
+const apiKey = "76337d09db3747c69a94b2fa9aa0b534";
 
 form.addEventListener("submit", e => {
     e.preventDefault();
+    const listItems = list.querySelectorAll(".ajax-section .city");
     const inputVal = input.value;
-});
 
-const apiKey = '76337d09db3747c69a94b2fa9aa0b534';
-const inputVal = input.value;
-const url = 'http://api.openweathermap.org/data/2.5/forecast?id=${inputVal}&appid=${apiKey}'
+  //ajax here
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
 
-fetch(url)
+    fetch(url)
     .then(response => response.json())
     .then(data => {
-        // do stuff with the data
+        const { main, name, sys, weather } = data;
+        const icon = `https://openweathermap.org/img/wn/${
+        weather[0]["icon"]
+        }@2x.png`;
+
+        const li = document.createElement("li");
+        li.classList.add("city");
+        const markup = `
+        <h2 class="city-name" data-name="${name},${sys.country}">
+            <span>${name}</span>
+            <sup>${sys.country}</sup>
+        </h2>
+        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+        <figure>
+            <img class="city-icon" src=${icon} alt=${weather[0]["main"]}>
+            <figcaption>${weather[0]["description"]}</figcaption>
+        </figure>
+        `;
+        li.innerHTML = markup;
+        list.appendChild(li);
     })
     .catch(() => {
-        msg.textContent = "Please search for a valid city";
+        msg.textContent = "Please search for a valid city 😩";
     });
 
-// Build the list items
-const {main, name, sys, weather } = data;
-const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
-    weather[0]["icon"]
-}.svg`;
-
-const li = document.createElement("li");
-li.classList.add("city");
-const markup = ` 
-<h2 class="city-name" data-name="${name},${sys.country}"> 
-<span>${name}</span> 
-<sup>${sys.country}</sup> 
-</h2> 
-<div class="city-temp">${Math.round(main.temp)}<sup>°C</sup> 
-</div> 
-<figure> 
-<img class="city-icon" src=${icon} alt=${weather[0]["main"]}> 
-<figcaption>${weather[0]["description"]}</figcaption> 
-</figure> 
-`;
-li.innerHTML = markup
-list.appendChild(li)
-
-msg.textContent = "";
-form.reset();
-input.focus();
+    msg.textContent = "";
+    form.reset();
+    input.focus();
+});
